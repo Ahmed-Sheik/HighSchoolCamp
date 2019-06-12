664d50c39546dad58b88e5747ff5500289bf508d
OrangeMethod Student <AhmedSheik680@gmail.com>
2019-06-12 11:47:28 -0500

    Added function excercises

---
+++ function_practice.py
@@ -0,0 +1,16 @@
+"""
+title: function_practice
+author: Ahmed
+date: 2019-06-12 11:22
+"""
+
+
+def age_calculator(year1, year2):
+    return abs(year1 - year2)
+
+print(age_calculator(2018, 1900))
+
+def avg_numbers(x, y, z)
+    return (x + y + z)/3
+
+print(avg_numbers(4, 7, 8))
