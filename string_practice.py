c9b16b8f809e635fb87beb991f5032924d25af12
OrangeMethod Student <AhmedSheik680@gmail.com>
2019-06-13 11:03:48 -0500

    Added string practice labs

--- string_practice.py
+++ string_practice.py
@@ -4,17 +4,33 @@ author: Ahmed
 date: 2019-06-11 13:45
 """
 
-print(answer.lower()in "qwertyuiopasdfghjklzxcvbnm")
+#print(answer.lower()in "qwertyuiopasdfghjklzxcvbnm")
 
-short = input("Enter a letter")
+#short = input("Enter a letter")
 
-short = short.lower().replace("and", "&")
-short = short.lower().replace("too", "2")
-short = short.().replace("you", "U")
-short = short.lower().replace("for", "4")
-short = short.().replace("a", "")
-short = short.().replace("e", "")
-short = short.().replace("i", "")
-short = short.().replace("o","")
-short = short.().replace("u",)
+#short = short.lower().replace("and", "&")
+#short = short.lower().replace("too", "2")
+#short = short.lower().replace("you", "U")
+#short = short.lower().replace("for", "4")
+#short = short.lower().replace("a", "")
+#short = short.lower().replace("e", "")
+#short = short.lower().replace("i", "")
+#short = short.lower().replace("o","")
+#short = short.lower().replace("u",)
 
+
+
+phrase =input("Enter a phrase: ")
+
+phrase =phrase.replace("'", "").replace(" ", "").replace("!", "").replace(".", "").replace("?", "").replace("&", "")
+
+print(phrase)
+
+def removing(check):
+    #check = check.lower().replace(" ", "")
+     #return check
+
+def palindrome(check):
+    check = removing(check)
+    return check == check [::-1]
+print(palindrome("Computer"))
